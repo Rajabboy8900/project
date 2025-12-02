cbf70e710e614bd72420f4395355e7913b14e1cb
Rajabboy <rajabbboyrajabov@gmail.com>
2025-12-02 09:07:54 +0500

    added

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,14 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-  // root: 'public',  <- O‘chirish kerak
   build: {
-    outDir: 'dist',  // root papka ichida dist hosil bo‘ladi
+    outDir: 'dist',
   },
   server: {
     port: 5173,
     host: true,
   },
 });
+
